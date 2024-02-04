3e28a2bebf530e0232bb2efcb657706123b27c36
samanthajiang <samanthajiang7@gmail.com>
2024-02-05 00:57:59 +0800

    main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import About from '../components/About';
 import Analytics from '../components/Analytics';
 import Canvas from '../components/Canvas';
@@ -10,44 +9,54 @@ import MainHero from '../components/MainHero';
 import MainHeroImage from '../components/MainHeroImage';
 import Pricing from '../components/Pricing';
 import Product from '../components/Product';
+import Upload_image from '../components/Upload_image';
+import Contact from '../components/Email';
+import Introduction from "../components/Intro";
 
 const App = () => {
   return (
     <div className={`bg-background grid gap-y-16 overflow-hidden`}>
-      <div className={`relative bg-background`}>
-        <div className="max-w-7xl mx-auto">
-          <div
-            className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
-          >
+      {/*<div className={`relative bg-background`}>*/}
+      {/*  <div className="max-w-7xl mx-auto">*/}
+      {/*    <div*/}
+      {/*      className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}*/}
+      {/*    >*/}
+      {/*      <Header />*/}
+      {/*      <MainHero />*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*  <MainHeroImage />*/}
+      {/*</div>*/}
+      {/*<Canvas />*/}
+      <LazyShow>
+        <>
             <Header />
-            <MainHero />
-          </div>
-        </div>
-        <MainHeroImage />
-      </div>
-      <Canvas />
-      <LazyShow>
-        <>
+
           <Product />
-          <Canvas />
+            <Introduction />
+          <Upload_image />
+
+          {/*<Canvas />*/}
         </>
       </LazyShow>
       <LazyShow>
         <>
-          <Features />
+
+
+            <Contact />
           <Canvas />
         </>
       </LazyShow>
-      <LazyShow>
-        <Pricing />
-      </LazyShow>
-      <LazyShow>
-        <>
-          <Canvas />
-          <About />
-        </>
-      </LazyShow>
-      <Analytics />
+      {/*<LazyShow>*/}
+      {/*  <Pricing />*/}
+      {/*</LazyShow>*/}
+      {/*<LazyShow>*/}
+      {/*  <>*/}
+      {/*    <Canvas />*/}
+      {/*    <About />*/}
+      {/*  </>*/}
+      {/*</LazyShow>*/}
+      {/*<Analytics />*/}
     </div>
   );
 };
